63216b10d02eba8dd5e8e61119df40d180cb0329
Scott Murray <scottmurray47@gmail.com>
2021-01-13 17:28:40 +1000

    challenge 1 first commit

---
+++ Challenge1/Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Scott Murray on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
