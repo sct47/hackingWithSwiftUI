4fa71d638f16bfebcefdad9bba271e21ac84c4b5
Scott Murray <scottmurray47@gmail.com>
2021-01-17 23:56:43 +1000

    project2 starter

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Scott Murray on 15/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = 
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image {
+                    
+                }
+                
+                Image {
+                    
+                }
+                
+                Image {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Scott Murray on 15/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
