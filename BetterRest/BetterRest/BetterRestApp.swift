ccbf031002f6c68f46ee265d78c6e62464ef7455
Scott Murray <scottmurray47@gmail.com>
2021-01-27 19:14:59 +1000

    better rest initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Scott Murray on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
